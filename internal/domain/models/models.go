1b6d3f2d9aa5cc57760bc000332ad17d003cd5ed
everpeznul <mafiramul@pvmpok.ru>
2026-02-15 20:39:56 +0300

    feat: add domain models

--- internal/domain/models/models.go
+++ internal/domain/models/models.go
@@ -1,1 +1,17 @@
 package models
+
+import "time"
+
+type Thread struct {
+	ChatID    int64
+	ThreadID  int64
+	Name      string
+	CreatedAt time.Time
+}
+
+type Message struct {
+	Thread    string
+	Text      string
+	Author    string
+	Timestamp time.Time
+}
